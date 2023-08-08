42330be1023be7995083a3cc93f42ba4d21188f8
Loïc Reynier <loic@loicreynier.fr>
2023-08-09 01:24:46 +0200

    feat(vscode-nvim): 2023-06-30 -> vscode-nvim-2023-08-08

--- pkgs/vim-plugins/vscode-nvim/default.nix
+++ pkgs/vim-plugins/vscode-nvim/default.nix
@@ -2,12 +2,12 @@
 
 vimUtils.buildVimPluginFrom2Nix {
   pname = "vscode.nvim";
-  version = "2023-06-30";
+  version = "2023-08-08";
   src = fetchFromGitHub {
     owner = "Mofiqul";
     repo = "vscode.nvim";
-    rev = "05973862f95f85dd0564338a03baf61b56e1823f";
-    hash = "sha256-iY3S3NnFH80sMLXgPKNG895kcWpl/IjqHtFNOFNTMKg=";
+    rev = "11b212096a0b2056fc8acaf31c0d62d656b6fd2d";
+    hash = "sha256-iwk1Nv+Q1Sc7Zpo3JQrN2QuHjVoTUlzM4S9OraT94P0=";
   };
   meta.homepage = "https://github.com/Mofiqul/vscode.nvim";
 }
