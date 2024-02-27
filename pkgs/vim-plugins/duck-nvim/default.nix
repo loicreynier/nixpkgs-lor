a6c591d52ea6747d93ee123350f9cacad925818b
Loïc Reynier <loic@loicreynier.fr>
2024-02-27 12:55:06 +0000

    feat(duck-nvim): 1.0.0 -> 1.0.0-unstable-2024-02-27

--- pkgs/vim-plugins/duck-nvim/default.nix
+++ pkgs/vim-plugins/duck-nvim/default.nix
@@ -1,13 +1,15 @@
-{ fetchFromGitHub, vimUtils }:
-
-vimUtils.buildVimPlugin rec {
+{ fetchFromGitHub
+, vimUtils
+,
+}:
+vimUtils.buildVimPlugin {
   pname = "duck-nvim";
-  version = "1.0.0";
+  version = "1.0.0-unstable-2024-02-27";
   src = fetchFromGitHub {
     owner = "tamton-aquib";
     repo = "duck.nvim";
-    rev = "v${version}";
-    hash = "sha256-G3n3viqsqlZ4Lo3jQY7cfEw7DmCKTCoxL4mvBBujOP0=";
+    rev = "0ca969d549f5d546ae395e163130024b51694235";
+    hash = "sha256-meATA9jsIrRUAZ+5PkznR8RADAC3KbmTHaG4p0oietw=";
   };
   meta.homepage = "https://github.com/tamton-aquib/duck.nvim";
 }
