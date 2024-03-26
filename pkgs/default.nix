db439feacdf712d6842af4a764cec5eff4d1eb41
Loïc Reynier <loic@loicreynier.fr>
2024-03-26 21:31:31 +0000

    feat(gh-f): init at 1.1.5

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,8 +3,9 @@
 with pkgs;
 {
   foxmarks = callPackage ./foxmarks { };
-  goto = callPackage ./goto { };
+  gh-f = callPackage ./gh-f { };
   gh-notify = callPackage ./gh-notify { };
+  goto = callPackage ./goto { };
   pinentry-wsl-ps1 = callPackage ./pinentry-wsl-ps1 { };
   pinentry-wsl-ps1-wsl2 = callPackage ./pinentry-wsl-ps1/wsl-2.nix { };
   spriggit-bin = callPackage ./spriggit-bin { };
